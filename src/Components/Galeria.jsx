4b873fe572dbb6e6f7fd1977842db5f3bdb8a1a0
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-06-10 14:30:39 -0300

    Galeria

--- src/Components/Galeria.jsx
+++ src/Components/Galeria.jsx
@@ -4,7 +4,6 @@ function Galeria(props){
     
     return(
         <div className="contenedor">
-            
             <img src="img/front-banner-1600_900.png" width= "100" alt="Guitar Shop"/>
             <img src="img/front-banner-1600_901.png" width= "100" alt="Guitar Shop"/>
             <img src="img/front-banner-1600_902.png" width= "100" alt="Guitar Shop"/>
@@ -13,7 +12,6 @@ function Galeria(props){
             <img src="img/front-banner-1600_905.png" width= "100" alt="Guitar Shop"/>
             <img src="img/front-banner-1600_906.png" width= "100" alt="Guitar Shop"/>
             <img src="img/front-banner-1600_907.png" width= "100" alt="Guitar Shop"/>
-        
         </div>
     )
 }
