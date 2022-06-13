35f88f0cba4da2813f151b44222e435376f288f6
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-06-13 12:51:40 -0300

    Galeria

--- src/Components/Galeria.jsx
+++ src/Components/Galeria.jsx
@@ -6,14 +6,14 @@ function Galeria(props){
     
     return(
         <div className="contenedor">
-            <img src="img/front-banner-1600_900.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_901.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_902.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_903.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_904.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_905.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_906.png" width= "100" alt="Guitar Shop"/>
-            <img src="img/front-banner-1600_907.png" width= "100" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_900.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_901.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_902.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_903.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_904.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_905.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_906.png" width= "150" alt="Guitar Shop"/>
+            <img src="img/front-banner-1600_907.png" width= "150" alt="Guitar Shop"/>
         </div>
     )
 }
