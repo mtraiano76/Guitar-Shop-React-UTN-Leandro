6814e11a73b55d3773c9ac41c62b9d52302b5327
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-07-06 14:28:18 -0300

    Botones de Home y Productos

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,18 @@
 import Button from 'react-bootstrap/Button'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+
 
 
 function Home(props) {
 
 
     return (
-        <div className="contenedor">
+        <div className="bienvenido">
             <img src="img/gibson.jpg" alt="Guitar Shop" />
             <div className="d-grid gap-2">
-                <Button as={Link} to={'/productos/'}variant="primary" size="lg">PRODUCTOS</Button>
-
-                <Button as={Link} to={'/galeria/'} variant="secondary" size="lg">GALERIA</Button>
-
-
-             
+                <Button as={Link} to={'/productos/'} variant="productos" size="lg">PRODUCTOS</Button>
+                <Button as={Link} to={'/galeria/'} variant="galeria" size="lg">GALERIA</Button>
             </div>
-
         </div>
     )
 }
